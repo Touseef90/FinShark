c86a75c3259993395129fa78264951f8725122ef
Touseef90 <touseef.ghazanfar@gmail.com>
2025-02-08 09:03:48 +0500

    Add Stock and Comment models with relationships - Creating a new model Stock.cs - Creating a new model Comment.cs - Created One to Many relation of Stock and Comments in Stock.cs file

---
+++ FinShark/Models/Comment.cs
@@ -0,0 +1,12 @@
+﻿namespace FinShark.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public int? StockId { get; set; } // Foreign key linking the comment to a specific stock
+        public Stock? Stock { get; set; } // Navigation property for accessing the related Stock entity i.e. different parts of stock
+    }
+}
