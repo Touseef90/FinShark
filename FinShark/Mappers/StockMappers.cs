19d9dc1faaa73f30e94657c62667ab66325290c7
Touseef90 <touseef.ghazanfar@gmail.com>
2025-02-09 21:48:08 +0500

    Dtos - Created Dtos folder and StockDto.cs file - Created Mappers folder and StockMappers.cs file - Update StockController.cs file used extention methods

---
+++ FinShark/Mappers/StockMappers.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Expressions;
+using FinShark.Dtos.Stock;
+using FinShark.Models;
+
+namespace FinShark.Mappers
+{
+    public static class StockMappers
+    {
+        public static StockDto ToStockDto(this Stock stockModel)
+        {
+            return new StockDto
+            {
+                Id = stockModel.Id,
+                Symbol = stockModel.Symbol,
+                CompanyName = stockModel.CompanyName,
+                Purchase = stockModel.Purchase,
+                LastDiv = stockModel.LastDiv,
+                Industry = stockModel.Industry,
+                MarketCap = stockModel.MarketCap
+            };
+        }
+    }
+}
