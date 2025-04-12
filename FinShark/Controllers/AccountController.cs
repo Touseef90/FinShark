ee13d73434269965304de0f569ce22e96b7bb474
Touseef90 <touseef.ghazanfar@gmail.com>
2025-04-12 07:17:36 +0500

    Login - Modified AccountController.cs file. Used SignInManager class to validate login credentials inside Login method - Added new LoginDto.cs file - Modified Program.cs file. Used AddSwaggerGen to add security token inside Swagger to Authorize

--- FinShark/Controllers/AccountController.cs
+++ FinShark/Controllers/AccountController.cs
@@ -6,6 +6,7 @@ using FinShark.Service;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FinShark.Controllers
 {
@@ -15,11 +16,51 @@ namespace FinShark.Controllers
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly ITokenService _tokenService;
+        private readonly SignInManager<AppUser> _signInManager;
 
-        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
+        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
             _tokenService = tokenService;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                var user = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username);
+
+                if (user == null)
+                {
+                    return Unauthorized("Invalid username or password!");
+                }
+
+                var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+                if (!result.Succeeded)
+                {
+                    return Unauthorized("Invalid username or password!");
+                }
+
+                return Ok(
+                    new NewUserDto
+                    {
+                        UserName = user.UserName,
+                        Email = user.Email,
+                        Token = _tokenService.CreateToken(user)
+                    }
+                );
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex);
+            }
         }
 
         [HttpPost("register")]
