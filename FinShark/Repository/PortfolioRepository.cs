68e57e30b93d0b36d8de51cffa34a27094ce6410
Touseef90 <touseef.ghazanfar@gmail.com>
2025-04-13 10:58:07 +0500

    Portfolio GET - Added new PortfolioController.cs, ClaimExtensions.cs, IPortfolioRepository.cs and PortfolioRepository.c files - Inside ClaimExtensions.cs added GetUsername extension method for ClaimsPrincipal - Modified Program.cs added PortfolioRepository as Scoped for Dependency Injection

---
+++ FinShark/Repository/PortfolioRepository.cs
@@ -0,0 +1,32 @@
+﻿using FinShark.Data;
+using FinShark.Interfaces;
+using FinShark.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinShark.Repository
+{
+    public class PortfolioRepository : IPortfolioRepository
+    {
+        private readonly ApplicationDBContext _context;
+
+        public PortfolioRepository(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Stock>> GetUserPortfolio(AppUser appUser)
+        {
+            return await _context.Portfolios.Where(u => u.AppUserId == appUser.Id)
+                .Select(stock => new Stock
+                {
+                    Id = stock.StockId,
+                    Symbol = stock.Stock.Symbol,
+                    CompanyName = stock.Stock.CompanyName,
+                    Purchase = stock.Stock.Purchase,
+                    LastDiv = stock.Stock.LastDiv,
+                    Industry = stock.Stock.Industry,
+                    MarketCap = stock.Stock.MarketCap
+                }).ToListAsync();
+        }
+    }
+}
